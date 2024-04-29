1c7a59cad64ff09b81da171bc62e1ef83f26ebd6
Shunkei3 <skakimoto3@huskers.unl.edu>
2024-04-29 14:51:15 -0500

    update umn xaringan temp

---
+++ umn_xaringan/gen_theme.r
@@ -0,0 +1,13 @@
+library(xaringanthemer)
+
+style_duo_accent(
+  header_h1_font_size = "1.8rem",
+  header_h2_font_size = "1.6rem",
+  header_h3_font_size = "1.4rem",
+  code_inline_background_color = "#e6e6e6", 
+  text_font_size = "1rem",
+  code_font_size = "1rem",
+  link_color = "#ff005a"
+)
+
+
